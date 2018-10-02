16cca557e873fe9138004c2f80cfabc67d2bbc3e
thelannisters <ntrung.hcmus@gmail.com>
2018-10-02 15:32:01 +0700

    abc

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ class App extends Component {
     super(props);
     this.state={
       hasMore: true,
-      isLoading: false,
       item: [],
+      isLoading: false,
       cursor: 1
     }
   }
