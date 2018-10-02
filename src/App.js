7aa0c260044f6fe4444ae8624e8eb5050ea9e744
thelannisters <ntrung.hcmus@gmail.com>
2018-10-02 20:51:54 +0700

    final

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-
 
 
 class App extends Component {
@@ -11,11 +9,10 @@ class App extends Component {
     this.state={
       hasMore: true,
       item: [],
-      isLoading: false,
       cursor: 1
     }
   }
-  componentDidMount(){
+ componentDidMount(){
     axios.get(`https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=210faf4ab82b8d0fdd0e13dc09080003&per_page=20&page=1&format=json&nojsoncallback=1&auth_token=72157701739829544-45eff7bf0469d894&api_sig=7b06914a1520a874d134fdaf507c6991`)
     .then((response)=>{
       
@@ -36,19 +33,17 @@ class App extends Component {
     })
     
   }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <p className="App-intro">
          {this.state.item}
          
         </p>
       </div>
-    );
+
+    )
   }
 }
 
